c239ec7ae6bcdd969bcc0c3a82a5f19dd5b0df12
Daniel Lovell <lovell.daniel92@gmail.com>
2021-01-10 01:15:44 -0800

    Use WeatherMin/MaxCooldownDuration instead of _defaultCooldown, support disabling weather completely

--- ConfigStormsPlugin.cs
+++ ConfigStormsPlugin.cs
@@ -9,7 +9,8 @@ namespace configurable_storms
     {
 
         public static bool HasWeather;
-        public static int DefaultCooldown;
+        public static int WeatherMinimumCooldownDuration;
+        public static int WeatherMaximumCooldownDuration;
         public static float StormWindStrength;
         public static float StormGroundedMaxSpeedMultiplier;
         public static float SolarPanelHealthDamage;
@@ -17,7 +18,8 @@ namespace configurable_storms
 
 
         private ConfigEntry<bool> configHW;
-        private ConfigEntry<int> configDC;
+        private ConfigEntry<int> configWMinCD;
+        private ConfigEntry<int> configWMaxCD;
         private ConfigEntry<float> configSWS;
         private ConfigEntry<float> configSGMSM;
         private ConfigEntry<float> configSPHD;
@@ -33,48 +35,53 @@ namespace configurable_storms
         void Awake()
         {
             HandleConfig();
-
-            ModLog("Successfully loaded Deep Mine Mod");
             ModLog("Patching...");
             var harmony = new Harmony("ConfigurableStorms");
             harmony.PatchAll();
             ModLog("Patched");
+            ModLog("Successfully loaded Configurable Storms");
         }
 
         void HandleConfig()
         {
             configHW = Config.Bind("Settings",   // The section under which the option is shown
-                                     "Weather Enabled",  // The key of the configuration option in the configuration file
+                                     "WeatherEnabled",  // The key of the configuration option in the configuration file
                                      true, // The default value
                                      "Enables weather on appropriate maps (default is true)"); // Description of the option to show in the config file
 
-            configDC = Config.Bind("Settings",   // The section under which the option is shown
-                                     "Default Storm Cooldown",  // The key of the configuration option in the configuration file
+            configWMinCD = Config.Bind("Settings",   // The section under which the option is shown
+                                     "WeatherMinimumCooldown",  // The key of the configuration option in the configuration file
                                      3, // The default value
-                                     "Cooldown between storms (default is 3)"); // Description of the option to show in the config file
+                                     "Lower bound of storm cooldown"); // Description of the option to show in the config file
+
+            configWMaxCD = Config.Bind("Settings",   // The section under which the option is shown
+                                     "WeatherMaximumCooldown",  // The key of the configuration option in the configuration file
+                                     12, // The default value
+                                     "Upper bound of storm cooldown"); // Description of the option to show in the config file
 
             configSWS = Config.Bind("Settings",   // The section under which the option is shown
-                                     "Storm Wind Strength",  // The key of the configuration option in the configuration file
+                                     "StormWindStrength",  // The key of the configuration option in the configuration file
                                      15f, // The default value
                                      "Strength of the storm's wind (default is 15"); // Description of the option to show in the config fil;
 
             configSGMSM = Config.Bind("Settings",   // The section under which the option is shown
-                                     "Storm Grounded Max Speed Multiplier",  // The key of the configuration option in the configuration file
+                                     "StormGroundedMaxSpeedMultiplier",  // The key of the configuration option in the configuration file
                                      0.1f, // The default value
                                      "Max possible multiplier to player speed when on the ground (default is 0.1"); // Description of the option to show in the config fil;
 
             configSPHD = Config.Bind("Settings",   // The section under which the option is shown
-                                     "Solar Panel Health Damage",  // The key of the configuration option in the configuration file
+                                     "SolarPanelHealthDamage",  // The key of the configuration option in the configuration file
                                      0.05f, // The default value
                                      "How much damage the storm does to solar panels (default is 0.05"); // Description of the option to show in the config fil;
 
             configDTHD = Config.Bind("Settings",   // The section under which the option is shown
-                                     "Dynamic Thing Health Damage",  // The key of the configuration option in the configuration file
+                                     "DynamicThingHealthDamage",  // The key of the configuration option in the configuration file
                                      0.03f, // The default value
                                      "How much damage the storm does to dynamic things, e.g. objects (default is 0.03"); // Description of the option to show in the config fil;
 
             HasWeather = configHW.Value;
-            DefaultCooldown = configDC.Value;
+            ModLog(HasWeather.ToString());
+            //DefaultCooldown = configDC.Value;
             StormWindStrength = configSWS.Value;
             StormGroundedMaxSpeedMultiplier = configSGMSM.Value;
             SolarPanelHealthDamage = configSPHD.Value;
--- Patches.cs
+++ Patches.cs
@@ -30,11 +30,56 @@ namespace configurable_storms
 
             static void Prefix(WeatherManager __instance)
             {
-                defaultCooldown.SetValue(__instance, ConfigStormsPlugin.DefaultCooldown);
+                //defaultCooldown.SetValue(__instance, ConfigStormsPlugin.DefaultCooldown);
                 stormWindStrength.SetValue(__instance, ConfigStormsPlugin.StormWindStrength);
                 stormGroundedMaxSpeedMultiplier.SetValue(__instance, ConfigStormsPlugin.StormGroundedMaxSpeedMultiplier);
                 dynamicThingsHealthDamage.SetValue(__instance, ConfigStormsPlugin.DynamicThingHealthDamage);
                 solarPanelHealthDamage.SetValue(__instance, ConfigStormsPlugin.SolarPanelHealthDamage);
+            }
+        }
+
+        /// <summary>
+        /// Min/max cooldown durations
+        /// </summary>
+        [HarmonyPatch(typeof(WeatherManager), "LoadWeatherEventSettings")]
+        public class WeatherManager_LoadWeatherEventSettings
+        {
+            static PropertyInfo worldHasWeather = AccessTools.Property(typeof(WeatherManager), "WorldHasWeather");
+
+            static void Postfix(WeatherManager __instance)
+            {
+                //defaultCooldown.SetValue(__instance, ConfigStormsPlugin.DefaultCooldown);
+                if(WeatherManager.Instance != null && WeatherManager.CurrentWeatherSetting != null)
+                {
+                    WeatherManager.CurrentWeatherSetting.WeatherMinimumCooldownDuration = ConfigStormsPlugin.WeatherMinimumCooldownDuration;
+                    WeatherManager.CurrentWeatherSetting.WeatherMaximumCooldownDuration = ConfigStormsPlugin.WeatherMaximumCooldownDuration;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(WeatherManager), "WorldHasWeather", MethodType.Getter)]
+        public class WeatherManager_WorldHasWeatherGetter
+        {
+            static void Postfix(ref bool __result)
+            {
+                if (__result == true)
+                {
+                    __result = ConfigStormsPlugin.HasWeather;
+                }
+            }
+        }
+
+        /// <summary>
+        /// For debugging
+        /// </summary>
+        [HarmonyPatch(typeof(WeatherManager), "OnWorldStart")]
+        public class WeatherManager_StartScheduleServer
+        {
+
+            static void Postfix(WeatherManager __instance)
+            {
+                //ConfigStormsPlugin.ModLog(WeatherManager.WeatherStartTime.ToString());
+                //ConfigStormsPlugin.ModLog(WeatherManager.IsWeatherEventScheduled.ToString());
             }
         }
     }
